ce1b3da995a9722d73649f4bc972a06aec28376b
RadarSun <hitsunzhicong@163.com>
2020-07-30 11:11:21 +0800

    加入版本界面

--- sr_hd_test_system.py
+++ sr_hd_test_system.py
@@ -13,6 +13,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 import sys
 import qtdesigner_rc
+import version_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -499,6 +500,104 @@ class Ui_Dialog(object):
         self.label8.setText(_translate("Dialog", "None"))
         self.label4.setText(_translate("Dialog", "None"))
         self.groupBox.setTitle(_translate("Dialog", "硬件信息"))
+
+
+class Ui_version_dialog(object):
+    def setupUi(self, dialog):
+        dialog.setObjectName("dialog")
+        dialog.resize(563, 430)
+        dialog.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"")
+        self.label = QtWidgets.QLabel(dialog)
+        self.label.setGeometry(QtCore.QRect(50, 10, 411, 171))
+        self.label.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"image: url(:/sr_new/sr_new.jpg);")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(dialog)
+        self.label_2.setGeometry(QtCore.QRect(100, 180, 111, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_2.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(dialog)
+        self.label_3.setGeometry(QtCore.QRect(100, 210, 271, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_3.setFont(font)
+        self.label_3.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_3.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(dialog)
+        self.label_4.setGeometry(QtCore.QRect(100, 240, 271, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_4.setFont(font)
+        self.label_4.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_4.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(dialog)
+        self.label_5.setGeometry(QtCore.QRect(100, 270, 201, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_5.setFont(font)
+        self.label_5.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_5.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(dialog)
+        self.label_6.setGeometry(QtCore.QRect(100, 300, 301, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_6.setFont(font)
+        self.label_6.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_6.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(dialog)
+        self.label_7.setGeometry(QtCore.QRect(100, 330, 301, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_7.setFont(font)
+        self.label_7.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_7.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(dialog)
+        self.label_8.setGeometry(QtCore.QRect(100, 360, 391, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_8.setFont(font)
+        self.label_8.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_8.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_8.setObjectName("label_8")
+
+        self.retranslateUi(dialog)
+        QtCore.QMetaObject.connectSlotsByName(dialog)
+
+    def retranslateUi(self, dialog):
+        _translate = QtCore.QCoreApplication.translate
+        dialog.setWindowTitle(_translate("dialog", "Version"))
+        self.label_2.setText(_translate("dialog", "Version 1.0"))
+        self.label_3.setText(_translate("dialog", "CopeRight  © 2020 Standard Robots"))
+        self.label_4.setText(_translate("dialog", "All rights reserved"))
+        self.label_5.setText(_translate("dialog", "Technical support:"))
+        self.label_6.setText(_translate("dialog", "SunZhicong, SUSTech, Shenzhen, China"))
+        self.label_7.setText(_translate("dialog", "Email:hitsunzhicong@163.com"))
+        self.label_8.setText(_translate("dialog", "Github: https://github.com/RadarSun/SR_work"))
         
 ##############################################################################################################
 #       PDF类
@@ -828,21 +927,19 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         
 
 ##############################################################################################################
-#       子界面的类
+#       操作界面的类
 ##############################################################################################################
 class ChildWin(QMainWindow, Ui_Dialog):
     #定义信号
-    _signal = QtCore.pyqtSignal(str)
     def __init__(self):
         super(ChildWin, self).__init__()
-        
+        self.VersionDialog = VersionWin()
         bar=self.menuBar()
         #向菜单栏中添加新的QMenu对象，父菜单
         helpbar=bar.addMenu('Help')
         helpbar.addAction('Help Document')
         about = helpbar.addMenu('About')
         about.addAction('Version')
-        
 
         #单击任何Qmenu对象，都会发射信号，绑定槽函数
         helpbar.triggered[QAction].connect(self.processtrigger)
@@ -856,7 +953,7 @@ class ChildWin(QMainWindow, Ui_Dialog):
         #输出那个Qmenu对象被点击
         print(q.text()+'is triggeres')
         if q.text() == "Version":
-            pass
+            self.VersionDialog.show()
         
     def connect_event(self):
         self.pushButton1.clicked.connect(self.onButton1Click) 
@@ -940,6 +1037,15 @@ class ChildWin(QMainWindow, Ui_Dialog):
             event.accept()
         else:
             event.ignore()
+            
+##############################################################################################################
+#       版本界面的类
+############################################################################################################## 
+class VersionWin(QMainWindow,Ui_version_dialog):
+    def __init__(self):
+        super(VersionWin, self).__init__()
+        self.setupUi(self)
+        
 ##############################################################################################################
 #       通过数据库判断是否为管理员
 ##############################################################################################################
