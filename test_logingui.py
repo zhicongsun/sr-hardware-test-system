a431c2f3b25840095e4855c206057c35d0e59547
RadarSun <hitsunzhicong@163.com>
2020-08-08 16:53:58 +0800

    新版登录界面

--- test_logingui.py
+++ test_logingui.py
@@ -7,7 +7,7 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+#最新
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
